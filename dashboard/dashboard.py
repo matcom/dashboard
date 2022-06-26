ccaf7dac2a5fb064a96322631b189395e5f3d164
Suilan Estévez Velarde <suilanestevez@gmail.com>
2022-06-26 15:50:13 -0400

    Update reqs

---
+++ dashboard/dashboard.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import streamlit as st
+
+
+def load_data():
+    return pd.read_excel(
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ1njbIUsTc9nN-TIC9QpDUY0L7Az0aRZ88NznYajzpUBaKA5vf5iaRg98HkOQHU3x9Kpqm7VLLpGp0/pub?output=xlsx",
+        sheet_name=None,
+    )
+
+
+data = load_data()
+
+st.dataframe(data['Publicaciones'])

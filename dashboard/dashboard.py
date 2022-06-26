bfc7cac9f74228989f58ef093f1d532493c168e2
Suilan Estévez Velarde <suilanestevez@gmail.com>
2022-06-26 16:04:27 -0400

    Update info

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,3 +1,4 @@
+from typing import Dict
 import pandas as pd
 import streamlit as st
 
@@ -6,7 +7,7 @@ st.set_page_config(page_title="MatCom Dashboard", page_icon=":star", layout="wid
 
 
 @st.experimental_memo
-def load_data():
+def load_data() -> Dict[str, pd.DataFrame]:
     return pd.read_excel(
         "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ1njbIUsTc9nN-TIC9QpDUY0L7Az0aRZ88NznYajzpUBaKA5vf5iaRg98HkOQHU3x9Kpqm7VLLpGp0/pub?output=xlsx",
         sheet_name=None,
@@ -15,4 +16,21 @@ def load_data():
 
 data = load_data()
 
-st.dataframe(data['Publicaciones'])
+
+@st.experimental_memo
+def convert_to_csv(sheet: str):
+    return data[sheet].to_csv().encode("utf8")
+
+
+with st.sidebar:
+    sheet = st.selectbox("Colección", data)
+
+    st.download_button(
+        "Descargar",
+        data=convert_to_csv(sheet),
+        file_name=f"{sheet}.csv",
+        mime="text/csv",
+    )
+
+with st.expander(f"Ver datos: {sheet}", False):
+    st.dataframe(data[sheet])

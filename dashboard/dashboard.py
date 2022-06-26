22b1672d35b6bb62dfb2dfded4e6d1ea686ce378
Suilan Estévez Velarde <suilanestevez@gmail.com>
2022-06-26 15:54:51 -0400

    Update

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,6 +2,10 @@ import pandas as pd
 import streamlit as st
 
 
+st.set_page_config(page_title="MatCom Dashboard", page_icon=":star", layout="wide")
+
+
+@st.experimental_memo
 def load_data():
     return pd.read_excel(
         "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ1njbIUsTc9nN-TIC9QpDUY0L7Az0aRZ88NznYajzpUBaKA5vf5iaRg98HkOQHU3x9Kpqm7VLLpGp0/pub?output=xlsx",

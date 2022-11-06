bcfc183006ba799c30e7e613d281cd9b89d5e0af
Alejandro Piad-Morffis <apiad@apiad.net>
2022-11-06 00:19:08 -0400

    Update lots of people

--- dashboard/import_csv.py
+++ dashboard/import_csv.py
@@ -3,4 +3,14 @@ from models import JournalPaper, Person
 
 df = pd.read_csv("/src/data/publications.csv")
 
-print(df)
+for i, row in df.iterrows():
+    authors = row['Autores'].split('\n')
+
+    for author in authors:
+        person = Person(name=author, institution="-")
+
+        for p2 in Person.all():
+            if p2.name == person.name:
+                continue
+
+        person.save()

0e39dc0da47d99e764fa357be1068f39977b5d8d
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-11-10 14:39:55 -0500

    feat: add function to count the thesis by advisor

--- dashboard/utils.py
+++ dashboard/utils.py
@@ -1,4 +1,17 @@
+from streamlit_agraph import agraph, Node, Edge, Config
 from random import randint
 
 def generate_widget_key() -> str:
     return str(randint(0, 1000000))
+
+
+def count_theses_by_advisor( theses ) -> any:
+    advisors = {}
+
+    for thesis in theses:
+        for advisor in thesis.advisors:
+            if advisor not in advisors:
+                advisors[advisor] = 0
+            advisors[advisor] += 1
+
+    return advisors

48ae0e1a85542465004fa809c8f7871d9896a3a0
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-11-10 14:40:22 -0500

    feat: add function to build advisors graph

--- dashboard/utils.py
+++ dashboard/utils.py
@@ -15,3 +15,32 @@ def count_theses_by_advisor( theses ) -> any:
             advisors[advisor] += 1
 
     return advisors
+
+def build_tutors_graph( advisors, theses ) -> any:
+    
+    nodes = []
+    edges = []
+    count_theses = count_theses_by_advisor( theses )
+
+    for advisor in advisors:
+        nodes.append(Node(
+            id=advisor,
+            size=25 + count_theses[advisor] * 3,
+        ))
+
+    for thesis in theses:
+        for advisor in thesis.advisors:
+            for advisor_2 in thesis.advisors:
+                if advisor == advisor_2:
+                    continue 
+                edges.append( Edge(
+                    source=advisor, 
+                    label=thesis.title, 
+                    target=advisor_2,
+                    directed=False,
+                    collapsible=False
+                ))
+
+    config = Config( width=1000, height=700 )
+
+    return agraph(nodes=nodes, edges=edges, config=config)

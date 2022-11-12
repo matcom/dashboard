1c2efa2ebf27e489bf9ef0f26d6ca2cb3f205bce
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-11-11 21:17:16 -0500

    feat: frist version to build the graph

--- dashboard/modules/graph.py
+++ dashboard/modules/graph.py
@@ -1,6 +1,8 @@
 from streamlit_agraph import agraph, Node, Edge, Config
 from modules.utils import darken_color, count_theses_by_advisor, count_theses_between_two_advisors
 
+from typing import List, Tuple
+
 def build_advisors_graph( advisors, theses ) -> any:
     
     nodes = []
@@ -34,4 +36,44 @@ def build_advisors_graph( advisors, theses ) -> any:
 
     config = Config( width=900, height=700 )
 
+    return agraph(nodes=nodes, edges=edges, config=config)
+
+def build_nodes_and_edges( publications: any ) -> Tuple[ List[Node], List[Edge] ]:
+    is_nodes = {}
+    nodes = []
+    edges = []
+    for publication in publications:
+        for author in publication.authors:
+            if author.name not in is_nodes:
+                is_nodes[author.name] = True
+                nodes.append( 
+                    Node(
+                    id=author.name,
+                    label=author.name,
+                    title=author.name,
+                    color='#ACDBC9',
+                    size=25,
+                ))
+
+            for author_2 in publication.authors:
+                if author != author_2:
+                    edges.append( 
+                        Edge(
+                            source=author.name, 
+                            label="1",
+                            target=author_2.name,
+                            color='#52FFCC',
+                            directed=False,
+                            collapsible=False
+                        ))
+    del is_nodes
+    return ( nodes, edges )
+
+
+def build_publications_graph( publications: any ) -> any:
+
+    nodes, edges = build_nodes_and_edges( publications )
+
+    config = Config( width=900, height=700 )
+    
     return agraph(nodes=nodes, edges=edges, config=config)

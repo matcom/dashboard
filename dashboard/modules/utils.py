851cb751be1362e73969c16f1cb6f2db1bb87795
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-11-12 16:09:16 -0500

    feat: add methods to count publications by persons

--- dashboard/modules/utils.py
+++ dashboard/modules/utils.py
@@ -1,5 +1,9 @@
+
 from random import randint
 import colorsys
+
+from models import Person
+from uuid import UUID
 
 def generate_widget_key() -> str:
     return str(randint(0, 1000000))
@@ -21,6 +25,24 @@ def count_theses_between_two_advisors( theses, advisor_1, advisor_2 ) -> int:
     for thesis in theses:
         if advisor_1 in thesis.advisors and advisor_2 in thesis.advisors:
             count += 1
+    return count
+
+def count_publications_between_two_persons( publications, person_1: Person, person_2: Person ) -> int:
+    count = 0
+    for publication in publications:
+        if person_1 in publication.authors and person_2 in publication.authors:
+            count += 1
+    return count
+
+def count_publications_by_person( publications ) -> dict[ UUID, int ]:
+    count: dict[ UUID, int ] = {}
+    
+    for publication in publications:
+        for author in publication.authors:
+            if author.uuid  not in count:
+                count[ author.uuid ] = 0
+            count[ author.uuid ] += 1
+
     return count
 
 def darken_color(color:str, number:int, range:int) -> str:

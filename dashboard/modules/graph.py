f7a953c234daf114f5fb05a384eb0ff6da1f9d10
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-11-12 16:40:36 -0500

    feat: show common publications

--- dashboard/modules/graph.py
+++ dashboard/modules/graph.py
@@ -1,12 +1,10 @@
 from streamlit_agraph import agraph, Node, Edge, Config
 from modules.utils import darken_color, count_theses_by_advisor, count_theses_between_two_advisors
+from modules.utils import count_publications_by_person, count_publications_between_two_persons
 
 from typing import List, Tuple
 from models import Person
 from uuid import UUID
-
-from random import randint
-import streamlit as st
 
 def build_advisors_graph( advisors, theses ) -> any:
     
@@ -43,28 +41,29 @@ def build_advisors_graph( advisors, theses ) -> any:
     return agraph(nodes=nodes, edges=edges, config=config)
 
 class NodeGraph:
-    def __init__(self, info: Person) -> None:
+    def __init__(self, info: Person, size = 25, color = '#ACDBC9') -> None:
         self.info = info
-        self.color = '#ACDBC9'
-        self.size = 25
+        self.color = color
+        self.size = size
 
 class EdgeGraph:
-    def __init__(self, source:NodeGraph, target:NodeGraph, info) -> None:
+    def __init__(self, source:NodeGraph, target:NodeGraph, info, color='#ACDBC9') -> None:
         self.source = source
         self.target = target
         self.info = info
-        self.color = '#ACDBC9'
+        self.color = color
 
 def build_nodes_and_edges( publications: any ) -> Tuple[ List[NodeGraph], List[EdgeGraph] ]:
     all_nodes: dict[UUID, Person] = {}
     nodes: List[NodeGraph] = []
     edges: List[EdgeGraph] = []
+    publications_by_person = count_publications_by_person( publications )
     
     for publication in publications:
         # save nodes
         for author in publication.authors:
             if author.uuid not in all_nodes:
-                nn = NodeGraph(author)
+                nn = NodeGraph(author, 25 + 3*publications_by_person[author.uuid])
                 all_nodes[ author.uuid ] = nn
                 nodes.append( nn )
 
@@ -82,20 +81,23 @@ def build_publications_graph( publications: List[any], width = 900, height = 700
     nodesGraph, edgesGraph = build_nodes_and_edges( publications )
     nodes: List[Node] = []
     edges: List[Edge] = []
-
+    publ_by_person = count_publications_by_person( publications )
+    
     for node in nodesGraph:
+        publ = f"{publ_by_person[ node.info.uuid ]} {'publicación' if publ_by_person[node.info.uuid] == 1 else 'publicaciones'}"
         nodes.append(Node( 
             id=f"{node.info.uuid}",
+            title=f"{node.info.name}\n{publ}",
             label=node.info.name,
-            title=node.info.name,
             color=node.color,
             size=25
         ))
         
     for edge in edgesGraph:
+        count_pub = count_publications_between_two_persons( publications, edge.source.info, edge.target.info )
         edges.append(Edge(
             source=f"{edge.source.info.uuid}", 
-            label=f"{5} publicación",
+            label=f"{count_pub} {'publicación' if count_pub == 1 else 'publicaciones'}",
             target=f"{edge.target.info.uuid}",
             color=edge.color,
             directed=False,
@@ -103,8 +105,8 @@ def build_publications_graph( publications: List[any], width = 900, height = 700
         ))
         edges.append(Edge(
             source=f"{edge.target.info.uuid}", 
-            label=f"{5} publicación",
             target=f"{edge.source.info.uuid}",
+            label="",
             color=edge.color,
             directed=False,
             collapsible=False
